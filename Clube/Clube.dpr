69e6f64bc30bad69980cf0d4c23504f7051a2981
Gustavo Santos <gustavodossantos764@gmail.com>
2021-01-23 12:20:10 -0400

    Adicionado menu e menuPopup no projeto

--- Clube/Clube.dpr
+++ Clube/Clube.dpr
@@ -2,12 +2,12 @@ program Clube;
 
 uses
   Forms,
-  UPrincipal in 'UPrincipal.pas' {Form1};
+  UPrincipal in 'UPrincipal.pas' {FormPrincipal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
   Application.Run;
 end.
